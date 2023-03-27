e02532176ea7135f55be1287ebb6cbff9bcb36cc
Gabriel Moreira <gsm.br2003@gmail.com>
2023-03-27 13:46:16 -0300

    chore: add fonts config

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts, DMSans_400Regular,
+} from '@expo-google-fonts/dm-sans'
+import {
+  DMSerifDisplay_400Regular,
+} from '@expo-google-fonts/dm-serif-display'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  })
+
+  if(!fontsLoaded) return null
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+  
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
