8b678fea8e27c0eb6d58c7fc0d3a9af88eaadf16
Gabriel Moreira <gsm.br2003@gmail.com>
2023-03-28 15:35:59 -0300

    feat: add form in product screen

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
-import { Platform, TouchableOpacity } from "react-native"
+import { Platform, ScrollView, TouchableOpacity } from "react-native"
 import { ButtonBack } from "../../components/ButtonBack"
 import { Photo } from "../../components/Photo"
-import { Container, DeleteLabel, Header, PickImageButton, Title, Upload } from "./styles"
+import { Container, DeleteLabel, Form, Header, InputGroup, InputGroupHeader, Label, MaxCharacters, PickImageButton, Title, Upload } from "./styles"
 import * as ImagePicker from 'expo-image-picker';
 import { InputPrice } from "../../components/InputPrice"
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
 
 
 export const Product = () => {
@@ -28,35 +30,61 @@ export const Product = () => {
 
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-        <Header>
-            <ButtonBack onPress={() => {}}/>
-            <Title>
-                Cadastrar
-            </Title>
-            <TouchableOpacity>
-                <DeleteLabel>
-                    Deletar
-                </DeleteLabel>
-            </TouchableOpacity>
-        </Header>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <Header>
+                <ButtonBack onPress={() => {}}/>
+                <Title>
+                    Cadastrar
+                </Title>
+                <TouchableOpacity>
+                    <DeleteLabel>
+                        Deletar
+                    </DeleteLabel>
+                </TouchableOpacity>
+            </Header>
 
-       <Upload>
-            <Photo 
-                uri={image}
-            />
-            <PickImageButton 
-                title="Carregar" 
-                type='secondary' 
-                onPress={handlePickerImage}
-            />
-       </Upload>
+        <Upload>
+                <Photo 
+                    uri={image}
+                />
+                <PickImageButton 
+                    title="Carregar" 
+                    type='secondary' 
+                    onPress={handlePickerImage}
+                />
+        </Upload>
 
-       <InputPrice size="P" />
-       <InputPrice size="M" />
-       <InputPrice size="G" />
+            <Form>
+                <InputGroup>
+                    <Label>Nome</Label>
+                    <Input />
+                </InputGroup>
 
-         
+                <InputGroup>
+                    <InputGroupHeader>
+                        <Label>Descrição</Label>
+                        <MaxCharacters>0 de 60 caracters</MaxCharacters>
+                    </InputGroupHeader>
 
+                    <Input
+                        multiline
+                        maxLength={60}
+                        style={{height: 80}}
+                    />
+                </InputGroup>
+
+                <InputGroup>
+                    <Label>Tamanho e preços</Label>
+                    <InputPrice size="P" />
+                    <InputPrice size="M" />
+                    <InputPrice size="G" />
+                </InputGroup>
+            
+                <Button 
+                    title='Cadastrar produto'
+                />
+            </Form>
+        </ScrollView>
     </Container>
   )
 }
