3ec8ada6f582c05fe434a9f77ba454c64b098f7c
Gabriel Moreira <gsm.br2003@gmail.com>
2023-03-29 11:21:19 -0300

    feat: add header in Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,25 @@
-import { View, Text } from 'react-native'
+import { TouchableOpacity } from 'react-native'
+import { Container, Greeting, GreetingEmoji, GreetingText, Header } from './styles'
+import {MaterialIcons } from '@expo/vector-icons'
+
+import happyEmoji from '../../assets/happyemoji.png'
+import { useTheme } from 'styled-components/native'
 
 export const Home = () => {
+  const {COLORS} = useTheme()
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Container>
+      <Header> 
+        <Greeting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Olá, Admin</GreetingText>
+        </Greeting>
+
+        <TouchableOpacity>
+          <MaterialIcons name='logout' color={COLORS.TITLE} size={24} />
+        </TouchableOpacity>
+      </Header>
+    </Container>
   )
 }
